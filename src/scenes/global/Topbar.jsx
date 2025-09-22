1bb930436586f1060e40ad14575766ddaac43973
Salman-khan125 <salmankhanuse@gmail.com>
2025-09-22 06:22:22 -0700

    #comment

---
+++ src/scenes/global/Topbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import{Box,IconButton, useTheme} from "@mui/material";
+import { useContext } from 'react';
+import{ColorModeContext, tokens } from"../../theme";
+import InputBase  from "@mui/material";
+import {
+  LightModeOutlined,
+  DarkModeOutlined,
+  NotificationsOutlined,
+  SettingsOutlined,
+  PersonOutlined,
+  SearchOutlined,
+} from "@mui/icons-material";
+
+
+const Topbar = () => {
+  return (
+    <div>
+      Topbar
+    </div>
+  )
+}
+
+export default Topbar
